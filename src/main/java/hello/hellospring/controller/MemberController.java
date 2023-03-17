6a2bb5e2693e5897a60db19e36b24d99f1119f77
YunJin Gwak <yunjin8931@naver.com>
2023-03-17 21:58:07 +0900

    :seedling: Add 2023-03-17 [Spring] git first commit

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,56 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.domain.MemberForm;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    /**
+     * 회원등록 화면
+     */
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    /**
+     * 회원등록
+     */
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    /**
+     * 회원조회
+     */
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
+
+}
